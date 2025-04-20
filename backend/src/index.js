1e90746bc8b01f855466ed96308d526ddbc9a4b5
Bartosz <bartosz.kowalczyk.email@gmail.com>
2025-04-20 23:10:38 +0200

    Dodany Dockerfile dla backendu

---
+++ backend/src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+app.use(express.json()); // Parsowanie JSON-ów
+
+// Testowy endpoint
+app.get('/ping', (req, res) => res.send('pong!'));
+
+// Uruchomienie serwera
+app.listen(3000, () => {
+  console.log('✅ Backend działa na porcie 3000');
+});
